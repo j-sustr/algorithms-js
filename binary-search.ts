43b457f66403e902c57b1b96fcb6bd91a87093e3
Jan Šustr <jan.sustr11@gmail.com>
2025-08-06 15:49:10 +0200

    fix binary search

--- binary-search.ts
+++ binary-search.ts
@@ -1,17 +1,23 @@
 
 
 export function binarySearch(compareFn: (idx: any) => number, length: number, startIdx = 0) {
+    let prevLeft = -1;
     let left = startIdx;
     let right = startIdx + length;
     let compareResult = 0;
     
     while (left < right) {
-        let currentIdx = (right - left) >> 1;
+        let currentIdx = (right + left) >> 1;
         let compareResult = compareFn(currentIdx);
 
         if (compareResult === 0) {
             return currentIdx;
         } else if (compareResult > 0) {
+            if (left === prevLeft) {
+                // The target value is not there
+                return currentIdx;
+            }
+            prevLeft = left;
             left = currentIdx;
         } else {
             right = currentIdx;
